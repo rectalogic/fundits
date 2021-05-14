8384eec77d92442c80c5fd420500bd09a4d6b110
Andrew Wason <rectalogic@rectalogic.com>
2021-05-14 11:54:33 -0400

    Boilerplate Slack app
    Based on https://github.com/slackapi/bolt-js/tree/main/examples/getting-started-typescript

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+      typescript: {
+        directory: './tsconfig.json',
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 'off',
+    'max-len': ["error", { "code": 150 }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'jsx': 'never',
+        'ts': 'never',
+        'tsx': 'never',
+      }
+   ]
+  },
+};
